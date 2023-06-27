7dfdbdd574df3edbe4b94d28c59de457d214420e
Adeluxx <getahunaderajew@gmail.com>
2023-06-27 03:01:55 -0700

    python-classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines the class Square"""
+
+
+class Square:
+    """Represents a square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines the class Square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize new Square.
+
+        Args:
+            size (int): Size of the new square.
+        """
+        self.__size = size
